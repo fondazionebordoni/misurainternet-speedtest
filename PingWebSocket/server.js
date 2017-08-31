594f91199efe7381db0d970d1ad121b1facc1e94
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-08-31 20:27:47 +0200

    rimossi file non necessari

--- PingWebSocket/server.js
+++
@@ -1,9 +0,0 @@
-var WebSocketServer = require('ws').Server;
-
-wss = new WebSocketServer({port: 8080});
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-    	console.log('Received ping message');
-        ws.send('');
-    });
-});
