84ef1232696c801aed0c15ff7f4a3c814f128fb4
Jerin George Mathew <jerin.edassery95@gmail.com>
2017-09-01 10:10:18 +0200

    Aggiunti controlli sulla richiesta GET lato server

--- Speedtest/server.js
+++ Speedtest/server.js
@@ -8,7 +8,7 @@ function generateTestData(bytes){
 }
 
 var server = http.createServer(function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*'); //modificare poi '*' con il sito di misurainternet
 	res.setHeader('Connection', 'keep-alive');
 
 	if(req.method==='POST'){
@@ -21,19 +21,39 @@ var server = http.createServer(function (req, res) {
 		console.log('Received message GET');
 		var url_parts = url.parse(req.url, true);
 		var query = url_parts.query;
-		var reqObj=(JSON.parse(query.data));
-		if (reqObj.request==='download' && reqObj.data_length){
-			var data= generateTestData(reqObj.data_length);
-			res.writeHead(200);
-			res.end(data);
+		try{
+			var reqObj=(JSON.parse(query.data));
+			if (reqObj.request && reqObj.request==='download' && reqObj.data_length && reqObj.data_length>0 && reqObj.data_length<52428800){ //per il momento fisso il limite massimo a 50MB di dati scaricabili
+				var data= generateTestData(reqObj.data_length);
+				res.writeHead(200);
+				res.end(data);
+			}
+			else{
+				console.log('Formato JSON non valido');
+				console.log(reqObj);
+				res.writeHead(404);
+				res.end();
+			}
 		}
+		catch(e){
+			console.log('Errore nel parsing del JSON spedito tramite GET');
+			console.log(e);
+			res.writeHead(404);
+			res.end();
+		}
+
 	}
 
 	else if(req.method==='OPTIONS'){
 		console.log('messaggio ricevuto OPTIONS')
 		res.setHeader('Access-Control-Allow-Methods', 'POST');
-		res.setHeader('Access-Control-Max-Age',600);
+		res.setHeader('Access-Control-Max-Age',600); // il client puo inviarmi altre richieste POST per 5 minuti prima di dover nuovamente mandarmi nuovamente una richiesta OPTIONS
 		res.writeHead(200);
+		res.end();
+	}
+	else{
+		console.log('Metodo ' +  req.method + ' non previsto');
+		res.writeHead(404);
 		res.end();
 	}
 
@@ -42,8 +62,8 @@ server.listen(8080);
 
 wss = new WebSocketServer({server: server});
 wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-    	console.log('Received ping message');
-        ws.send('');
-    });
+	ws.on('message', function(message) {
+		console.log('Received ping message');
+		ws.send('');
+	});
 });
