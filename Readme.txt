dbc48a5c7a7e3407f5e9dc0d91ae60233189d7f6
Luca Pellegrini <aroldo93@hotmail.it>
2018-04-07 11:58:27 +0200

    Aggiunto il supporto provvisorio al multi-porta. Migliorate le performace del test di download (che non erano sufficienti dalla 200mb in su), al costo di non poter impostare la dimensione del blob in base al parametro della richiesta GET.

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+Passi da seguire per creare l'ambiente di sviluppo misurainternet-speedtest
+1- installare
+	1.1- git
+	1.2- nodejs (con npm)
+2- clonare il progetto da github con 'git clone'
+3- nella root del progetto lanciare il comando 'npm init' e lasciare tutti
+   valori di default per inizializzare il file 'package.json'
+4- lanciare il comando 'npm install ws'
--- Speedtest/server.js
+++ Speedtest/server.js
@@ -1,15 +1,28 @@
 var http = require('http');
 var crypto = require('crypto');
 var url = require('url');
+var events = require('events');
 var WebSocketServer = require('ws').Server;
 
 function generateTestData(bytes){
 	return crypto.randomBytes(bytes);
 }
 
-var server = http.createServer(function (req, res) {
+var m50 = 52428800;
+var m1 = 1048576;
+var m5 = 5242880;
+var m10 = 5242880*2;
+var m25 = m50/2;
+var data = generateTestData(m50);
+
+var serverFunc = function (req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*'); //modificare poi '*' con il sito di misurainternet
 	res.setHeader('Connection', 'keep-alive');
+	
+	var eventEmitter = new events.EventEmitter();
+	eventEmitter.on('data', function(buffer) {
+		console.log('bytes' + Buffer.byteLength(buffer));
+	});
 
 	if(req.method==='POST'){
 		console.log('Received message POST');
@@ -23,8 +36,8 @@ var server = http.createServer(function (req, res) {
 		var query = url_parts.query;
 		try{
 			var reqObj=(JSON.parse(query.data));
-			if (reqObj.request && reqObj.request==='download' && reqObj.data_length && reqObj.data_length>0 && Number.isInteger(reqObj.data_length) && reqObj.data_length<=52428800){ //per il momento fisso il limite massimo a 50MB di dati scaricabili
-				var data= generateTestData(reqObj.data_length);
+			if (reqObj.request && reqObj.request==='download' && reqObj.data_length && reqObj.data_length>0 && Number.isInteger(reqObj.data_length) && reqObj.data_length<=52428800*2){ //per il momento fisso il limite massimo a 50MB di dati scaricabili
+				//var data = generateTestData(reqObj.data_length);
 				res.writeHead(200);
 				res.end(data);
 			}
@@ -58,10 +71,30 @@ var server = http.createServer(function (req, res) {
 		res.end();
 	}
 
-})
-server.listen(process.argv[2]);
+}
 
-wss = new WebSocketServer({server: server});
+var server01 = http.createServer(serverFunc);
+var server02 = http.createServer(serverFunc);
+var server03 = http.createServer(serverFunc);
+var server04 = http.createServer(serverFunc);
+var server05 = http.createServer(serverFunc);
+var server06 = http.createServer(serverFunc);
+var server07 = http.createServer(serverFunc);
+var server08 = http.createServer(serverFunc);
+var server09 = http.createServer(serverFunc);
+var server10 = http.createServer(serverFunc);
+server01.listen(process.argv[2]);
+server02.listen(60101);
+server03.listen(60102);
+server04.listen(60103);
+server05.listen(60104);
+server06.listen(60105);
+server07.listen(60106);
+server08.listen(60107);
+server09.listen(60108);
+server10.listen(60109);
+
+wss = new WebSocketServer({server: server01});
 wss.on('connection', function(ws) {
 	ws.on('message', function(message) {
 		console.log('Received ping message');
